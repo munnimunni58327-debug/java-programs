d5250c272a2153ea2514e15f16a73f83a65dfd58
munnimunni58327-debug <munnimunni58327@gmail.com>
2026-07-01 19:37:07 +0530

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,12 @@
+public class Test2
+{
+	public static void main(String args[])
+	{
+	 	double d=10.6;
+	 	long l=(long) d;
+	 	int i=(int) d;
+	 	System.out.println("double value="+d);
+	 	System.out.println("long value="+l);
+	 	System.out.println("int value="+i);
+  	}
+}
