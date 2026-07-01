9994c6661777930cb9bb57be08640f0807cd32ef
munnimunni58327-debug <munnimunni58327@gmail.com>
2026-07-01 19:17:34 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test
+{
+	public static void main(String[] args)
+	 {
+	 	int i=100;
+	 	long l=i;
+	 	float f=i;
+	 	double d=i;
+	 	System.out.println("Integer value="+i);
+	 	System.out.println("long value="+l);
+	 	System.out.println("float value="+f);
+	 	System.out.println("double value="+d);
+  	}
+}
